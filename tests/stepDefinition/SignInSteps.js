f4002133b7d971331c97b98f4eaec1c0fd23cc59
Supriya <supusa.kho@gmail.com>
2025-06-13 16:35:44 -0500

    Update Sign In Feature

--- tests/stepDefinition/SignInSteps.js
+++ tests/stepDefinition/SignInSteps.js
@@ -14,25 +14,30 @@ When('The user should be able to land on Manan portal with Title {string}', asyn
 });
 
 When('User Click on Sign in button', async ({page}) => {
-  await page.getByRole('button', { name: 'Sign In' }).click();
+await page.getByRole('button', { name: 'Sign In' }).nth(0).click();
 });
 
-Then('User should navingate to Sign in pop up window.', async ({}) => {
-  // Step: Then User should navingate to Sign in pop up window.
-  // From: tests\features\SignIn.feature:8:1
+Then('User should navingate to Sign in pop up window.', async ({page}) => {
+    await page.waitForSelector('div[role="dialog"]');
+  // Check for welcome message
+ const welcomeHeading = page.locator('h2:has-text("Welcome to MANAN")');
+  await expect(welcomeHeading).toBeVisible();
 });
 
-When('When User enters valid username {string} and valid password {string}', async ({}, arg, arg1) => {
-  // Step: When When User enters valid username "NewSdetuser2" and valid password "Abcd1234!"
-  // From: tests\features\SignIn.feature:9:1
+When('When User enters valid username {string} and valid password {string}', async ({page}, UN, Pwd) => {
+  await page.getByPlaceholder('Enter your username').fill(UN);
+  await page.getByPlaceholder('Enter your password').fill(Pwd);
 });
 
-When('User click on SignIn button', async ({}) => {
-  // Step: And User click on SignIn button
-  // From: tests\features\SignIn.feature:10:1
+When('User click on SignIn button', async ({page}) => {
+  await page.getByRole('button', { name: 'Sign In' }).first().click();
+  await page.pause();
 });
 
-Then('Then User should be redirected to Manan App page', async ({}) => {
-  // Step: Then Then User should be redirected to Manan App page
-  // From: tests\features\SignIn.feature:11:1
+Then('Then User should be redirected to Manan App page {string}', async ({page}, appPage) => {
+ page.once('dialog', async dialog => {
+  await dialog.accept(); // Clicks OK
+ });
+ await expect(page).toHaveURL(appPage);
+
 });
