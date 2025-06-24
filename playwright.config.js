90e9c032fdfecb641c12a3c85aca69008d81577d
Supriya <supusa.kho@gmail.com>
2025-06-24 09:45:27 -0500

    all page code

--- playwright.config.js
+++ playwright.config.js
@@ -10,8 +10,8 @@ dotenv.config({
 
 const testDir = defineBddConfig({
 
-  //features: 'tests/features/***.feature',
-   features: ['tests/features/MananFormPage.feature'],
+  features: 'tests/features/***.feature',
+   //features: ['tests/features/MananFormPage.feature'],
   // features: ['tests/features/HomePage.feature', 'tests/features/HomePageSignIN.feature', 'tests/features/Dashboard.feature'],
   steps: ['tests/stepDefinition/***steps.js', 'tests/fixtures/Fixtures.js'
   ],
